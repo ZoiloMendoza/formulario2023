4f1cf551223e59de5c5ca038497f5fa602032478
ZoiloMendoza <zoilomm@hotmail.com>
2023-01-18 12:20:43 -0600

    version final

---
+++ main.js
@@ -0,0 +1,37 @@
+const form = document.querySelector('form');
+const submit = document.querySelector('button');
+
+const validacion = (objetoFinal) => {
+    let valores = Object.values(objetoFinal)
+    let claves = Object.keys(objetoFinal)
+
+    let output = valores.every((elemento) => !elemento == '');
+
+    if(output && claves.includes('lenguajes')){
+        return objetoFinal
+    }else{
+        return `Oye te falta completar el formulario ${valores[0]} e.e`
+    }
+}
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    let objetoFinal = {}
+    let arreglo = [];
+    Array.from(form).forEach((elemento) =>{
+        if(elemento.name != 'button' && elemento.name !='gender' && elemento.name != 'lenguajes'){
+            objetoFinal[elemento.name] = elemento.value;
+        }else if(elemento.name ==='gender' && elemento.checked){
+            objetoFinal[elemento.name] = elemento.value;
+        }else if(elemento.name ==='lenguajes' && elemento.checked){
+            arreglo.push(elemento.value)
+            objetoFinal[elemento.name] = [].concat(arreglo);
+        }
+        
+    })
+    
+    
+    console.log(validacion(objetoFinal));
+    
+})
+
